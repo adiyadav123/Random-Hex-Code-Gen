3ac3fdf1824368c348a05a00493f8f16724983a4
Aditya Yadav <103302549+adiyadav123@users.noreply.github.com>
2023-01-13 20:33:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const changeBg = () => {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+    color.innerHTML = "#" + randomColor;
+  }
+  
+  NewCol.addEventListener("click", changeBg);
+  changeBg();
